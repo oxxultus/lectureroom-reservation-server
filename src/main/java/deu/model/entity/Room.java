37a3f6ded5dc8c6a445f8c94cc6ebdb250fb8716
oixikite <siyeon425@naver.com>
2025-05-14 23:37:07 +0900

    add Room entity

---
+++ src/main/java/deu/model/entity/Room.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package deu.model.entity;
+
+/**
+ *
+ * @author oixikite
+ */
+public class Room {
+    private String building;
+    private int floor;
+    private int number;
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
