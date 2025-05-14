6fd854626ef7c6e042caced23d40a724e45c9fa1
oixikite <siyeon425@naver.com>
2025-05-14 23:36:03 +0900

    add Lecture entity

---
+++ src/main/java/deu/model/entity/Lecture.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package deu.model.entity;
+
+import java.util.List;
+
+/**
+ *
+ * @author oixikite
+ */
+public class Lecture {
+    private String lec_id;
+    private String name;
+    private String professor;
+    private Room room;
+    private String day;
+    private List<Integer> periods;
+
+    public String getLec_id() {
+        return lec_id;
+    }
+
+    public void setLec_id(String lec_id) {
+        this.lec_id = lec_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(String professor) {
+        this.professor = professor;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public List<Integer> getPeriods() {
+        return periods;
+    }
+
+    public void setPeriods(List<Integer> periods) {
+        this.periods = periods;
+    }
+}
