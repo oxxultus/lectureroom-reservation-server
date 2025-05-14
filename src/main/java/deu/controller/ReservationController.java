d9bfbd8fbf4d1fe76d9974ffcd1db409a0a83507
oxxultus <a38027928@gmail.com>
2025-05-14 22:01:35 +0900

    write controller description

--- src/main/java/deu/controller/ReservationController.java
+++ src/main/java/deu/controller/ReservationController.java
@@ -4,6 +4,7 @@ import deu.model.dto.request.ReservationRequest;
 import deu.model.dto.response.BasicResponse;
 import deu.service.ReservationService;
 
+// 해당 컨트롤러는 예약 컨트롤러 입니다.
 public class ReservationController {
     private static final ReservationController instance = new ReservationController();
     private final ReservationService reservationService = new ReservationService();
