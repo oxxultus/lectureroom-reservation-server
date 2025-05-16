31ca4415536d53b4caef58a0111b7c8c7af41fec
oxxultus <a38027928@gmail.com>
2025-05-16 20:11:24 +0900

    Edit reservation controller

--- src/main/java/deu/controller/ReservationController.java
+++ src/main/java/deu/controller/ReservationController.java
@@ -1,13 +1,10 @@
 package deu.controller;
 
-import deu.model.dto.request.ReservationRequest;
-import deu.model.dto.response.BasicResponse;
 import deu.service.ReservationService;
 
-// 해당 컨트롤러는 예약 컨트롤러 입니다.
+// 예약 컨트롤러
 public class ReservationController {
     private static final ReservationController instance = new ReservationController();
-    private final ReservationService reservationService = new ReservationService();
 
     private ReservationController() {}
 
@@ -15,7 +12,6 @@ public class ReservationController {
         return instance;
     }
 
-    public BasicResponse reserve(ReservationRequest payload) {
-        return reservationService.reserve(payload);
-    }
+    private final ReservationService reservationService = new ReservationService();
+
 }
