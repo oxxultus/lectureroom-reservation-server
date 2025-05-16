0f923b48d5c97b70728fa77b92e185199d5e5ae4
이건일 <lgi0507@naver.com>
2025-05-16 13:39:08 +0900

    생성자

--- src/main/java/deu/repository/UserRepositoryImpl.java
+++ src/main/java/deu/repository/UserRepositoryImpl.java
@@ -6,12 +6,15 @@ import java.util.List;
 import java.io.*;
 import java.util.ArrayList;
 
+import static com.sun.tools.jdeprscan.DeprDB.loadFromFile;
+
 public class UserRepositoryImpl {
     //저장경로
     private static UserRepositoryImpl instance;
 
     // TODO: final 변수는 초기화를 진행해야 합니다.
-    // private final List<User> users;
+    private final List<User> users;
+
     private final String filePath = "user_data.ser";
 
     // singleton 패턴으로 구현한다
@@ -22,20 +25,12 @@ public class UserRepositoryImpl {
         return instance;
     }
     // TODO: 아래 코드는 중복 된 코드입니다. 싱글톤 패턴에서는 static으로 메서드를 구현해야 합니다.
-    /*
-        public UserRepositoryImpl getInstance() {
-
-            if (instance == null) {
-                instance = new UserRepositoryImpl();
-            }
-            return instance;
-        }
-    */
-
 
     // 생성자
-
-
+    private UserRepositoryImpl() {
+        this.users = new ArrayList<>();
+        loadFromFile();
+    }
     // 파일에서 읽어오는 메서드
 
 
