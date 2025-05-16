55aeac0424a6c58b47162eb6a6bbccf315a67920
oxxultus <a38027928@gmail.com>
2025-05-16 19:49:41 +0900

    add ReservationCommandRequest.java

---
+++ src/main/java/deu/model/dto/request/command/ReservationManagementCommandRequest.java
@@ -0,0 +1,23 @@
+package deu.model.dto.request.command;
+
+import java.io.Serializable;
+
+/**
+ * [ReservationManagementCommandRequest 클래스]
+ * <p>
+ * 이 클래스는 강의 관련 요청을 서버에 전달하기 위한 커맨드 기반 DTO입니다.
+ * 커맨드(command) 필드에는 요청의 유형(예: "create", "delete", "update" 등)이 들어가고,
+ * payload에는 해당 요청에 필요한 데이터를 객체 형태로 담습니다.
+ * </p>
+ *
+ * <p>직렬화를 위해 Serializable을 구현하고 있습니다.</p>
+ */
+public class ReservationManagementCommandRequest implements Serializable {
+    public String command;
+    public Object payload;
+
+    public ReservationManagementCommandRequest(String command, Object payload) {
+        this.command = command;
+        this.payload = payload;
+    }
+}
