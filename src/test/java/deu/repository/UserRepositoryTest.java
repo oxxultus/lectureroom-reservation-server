7a28ff41987d36e1f1d722595e3cc7bd9a024233
oxxultus <a38027928@gmail.com>
2025-05-19 15:15:16 +0900

    Test code modification according to the repository class modification

--- src/test/java/deu/repository/UserRepositoryTest.java
+++ src/test/java/deu/repository/UserRepositoryTest.java
@@ -3,6 +3,7 @@ package deu.repository;
 import deu.model.dto.response.BasicResponse;
 import deu.model.entity.User;
 import org.junit.jupiter.api.*;
+
 import java.io.File;
 import java.util.List;
 
@@ -13,10 +14,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UserRepositoryTest {
 
     private UserRepository repo;
-    private final String TEST_ID = "T2025";
+    private final String TEST_ID = "test001";
     private final String TEST_PASSWORD = "pw123";
-    private final String TEST_NAME = "김영진";
-    private final String TEST_MAJOR = "컴퓨터소프트웨어공학과";
+    private final String TEST_NAME = "테스트 유저";
+    private final String TEST_MAJOR = "소프트웨어학과";
 
     private final String DATA_DIR_PATH = System.getProperty("user.dir") + File.separator + "data";
     private final String TEST_FILE_PATH = DATA_DIR_PATH + File.separator + "users.yaml";
@@ -35,10 +36,7 @@ public class UserRepositoryTest {
 
         File dir = new File(DATA_DIR_PATH);
         if (dir.exists() && dir.isDirectory() && dir.list().length == 0) {
-            boolean deleted = dir.delete();
-            if (deleted) {
-                System.out.println("[TestCleanup] data 디렉토리 삭제됨: " + dir.getAbsolutePath());
-            }
+            dir.delete();
         }
     }
 
@@ -49,109 +47,109 @@ public class UserRepositoryTest {
 
     @Test
     @Order(1)
-    void testSaveSuccess() {
-        BasicResponse response = repo.save(TEST_ID, TEST_PASSWORD, TEST_NAME, TEST_MAJOR);
-        assertEquals("200", response.code);
+    void testSaveUser() {
+        BasicResponse res = repo.save(TEST_ID, TEST_PASSWORD, TEST_NAME, TEST_MAJOR);
+        assertEquals("200", res.code);
     }
 
     @Test
     @Order(2)
-    void testSaveDuplicate() {
+    void testDuplicateUserSave() {
         repo.save(TEST_ID, TEST_PASSWORD, TEST_NAME, TEST_MAJOR);
-        BasicResponse response = repo.save(TEST_ID, "another", "이름", "전공");
-        assertEquals("400", response.code);
+        BasicResponse res = repo.save(TEST_ID, "otherpw", "다른이름", "다른학과");
+        assertEquals("400", res.code);
     }
 
     @Test
     @Order(3)
     void testValidateSuccess() {
         repo.save(TEST_ID, TEST_PASSWORD, TEST_NAME, TEST_MAJOR);
-        BasicResponse response = repo.validate(TEST_ID, TEST_PASSWORD);
-        assertEquals("200", response.code);
+        BasicResponse res = repo.validate(TEST_ID, TEST_PASSWORD);
+        assertEquals("200", res.code);
     }
 
     @Test
     @Order(4)
     void testValidateWrongPassword() {
         repo.save(TEST_ID, TEST_PASSWORD, TEST_NAME, TEST_MAJOR);
-        BasicResponse response = repo.validate(TEST_ID, "wrong");
-        assertEquals("401", response.code);
+        BasicResponse res = repo.validate(TEST_ID, "wrongpw");
+        assertEquals("401", res.code);
     }
 
     @Test
     @Order(5)
     void testValidateNonexistentUser() {
-        BasicResponse response = repo.validate("NONE", "pw");
-        assertEquals("400", response.code);
+        BasicResponse res = repo.validate("none", "pw");
+        assertEquals("400", res.code);
     }
 
     @Test
     @Order(6)
     void testFindByNumberSuccess() {
         repo.save(TEST_ID, TEST_PASSWORD, TEST_NAME, TEST_MAJOR);
-        BasicResponse response = repo.findByNumber(TEST_ID);
-        assertEquals("200", response.code);
-        assertTrue(response.data instanceof User);
+        BasicResponse res = repo.findByNumber(TEST_ID);
+        assertEquals("200", res.code);
+        assertTrue(res.data instanceof User);
     }
 
     @Test
     @Order(7)
     void testFindByNumberFail() {
-        BasicResponse response = repo.findByNumber("UNKNOWN");
-        assertEquals("404", response.code);
+        BasicResponse res = repo.findByNumber("unknown");
+        assertEquals("404", res.code);
     }
 
     @Test
     @Order(8)
-    void testUpdateSuccess() {
+    void testUpdateUser() {
         repo.save(TEST_ID, TEST_PASSWORD, TEST_NAME, TEST_MAJOR);
-        BasicResponse response = repo.update(TEST_ID, "newpw", "이순신", "AI학과");
-        assertEquals("200", response.code);
+        BasicResponse res = repo.update(TEST_ID, "newpw", "변경된이름", "AI학과");
+        assertEquals("200", res.code);
     }
 
     @Test
     @Order(9)
-    void testUpdateFail() {
-        BasicResponse response = repo.update("NOPE", "pw", "이름", "전공");
-        assertEquals("404", response.code);
+    void testUpdateNonexistentUser() {
+        BasicResponse res = repo.update("notExist", "pw", "이름", "전공");
+        assertEquals("404", res.code);
     }
 
     @Test
     @Order(10)
-    void testDeleteSuccess() {
+    void testDeleteUser() {
         repo.save(TEST_ID, TEST_PASSWORD, TEST_NAME, TEST_MAJOR);
-        BasicResponse response = repo.deleteByNumber(TEST_ID);
-        assertEquals("200", response.code);
+        BasicResponse res = repo.deleteByNumber(TEST_ID);
+        assertEquals("200", res.code);
     }
 
     @Test
     @Order(11)
-    void testDeleteFail() {
-        BasicResponse response = repo.deleteByNumber("NOT_EXIST");
-        assertEquals("404", response.code);
+    void testDeleteNonexistentUser() {
+        BasicResponse res = repo.deleteByNumber("notExist");
+        assertEquals("404", res.code);
     }
 
     @Test
     @Order(12)
     void testFindAll() {
         repo.save(TEST_ID, TEST_PASSWORD, TEST_NAME, TEST_MAJOR);
-        BasicResponse response = repo.findAll();
-        assertEquals("200", response.code);
-        assertTrue(response.data instanceof List);
+        BasicResponse res = repo.findAll();
+        assertEquals("200", res.code);
+        assertTrue(res.data instanceof List);
     }
 
     @Test
     @Order(13)
-    void testExistsTrue() {
+    void testExistsByNumberTrue() {
         repo.save(TEST_ID, TEST_PASSWORD, TEST_NAME, TEST_MAJOR);
-        BasicResponse response = repo.existsByNumber(TEST_ID);
-        assertEquals("200", response.code);
+        BasicResponse res = repo.existsByNumber(TEST_ID);
+        assertEquals("200", res.code);
     }
 
     @Test
     @Order(14)
-    void testExistsFalse() {
-        BasicResponse response = repo.existsByNumber("NON_USER");
-        assertEquals("404", response.code);
+    void testExistsByNumberFalse() {
+        BasicResponse res = repo.existsByNumber("notExist");
+        assertEquals("404", res.code);
     }
 }
