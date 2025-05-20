b85a0001a9a938e9f25b0441cd9da87837ffbe4f
Guddnjs02 <dnjsgud0123@naver.com>
2025-05-21 03:57:51 +0900

    add method handleGetUpcomingReservationsByUser(), handleGetUpcomingAllReservations() in ReservationController

--- src/main/java/deu/controller/business/ReservationController.java
+++ src/main/java/deu/controller/business/ReservationController.java
@@ -6,7 +6,6 @@ import deu.service.ReservationService;
 
 import java.util.List;
 
-
 // 예약 컨트롤러
 public class ReservationController {
     private static final ReservationController instance = new ReservationController();
@@ -32,6 +31,11 @@ public class ReservationController {
 
     public BasicResponse handleDeleteReservation(String userId, String date, String startTime) {
         return reservationService.deleteReservation(userId, date, startTime);
+    }
+
+    // 주간 예약 시간표 반환
+    public RoomReservation[][] handleGetWeeklyReservations(String building, String floor, String room) {
+        return reservationService.getWeeklyReservations(building, floor, room);
     }
 
     // 각 메서드 앞에 handle를 꼭 붙혀주세요 (알관성 유지)
